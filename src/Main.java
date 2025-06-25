24c6615da0a21df093ddce5ac3f71ce9a1404106
Леонид <lenya.samoyilov2011@yandex.ru>
2025-06-26 00:32:37 +0400

    Готово на проверку.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        TaskManager taskManager = new TaskManager();
+
+        Task taskNew = new Task("Собрать коробки", "Положить в багажник");
+        Task taskUpdate = taskManager.addTask(taskNew);
+        System.out.println(taskUpdate);
+
+        Task taskCreate = new Task(taskNew.getId(), "Пойти поработать", "Взять еду",
+                Status.IN_PROGRESS);
+        Task taskNewCreate = taskManager.updateTask(taskCreate);
+        System.out.println(taskNewCreate);
+
+
+        Epic flagUpdate = new Epic("Поехать на отдых", "Не забыть жену");
+        taskManager.addEpic(flagUpdate);
+        System.out.println(flagUpdate);
+        Subtask flagUpdateSubtask1 = new Subtask("Купить продукты", "Взять список",
+                flagUpdate.getId());
+        Subtask flagUpdateSubtask2 = new Subtask("Провести уборку", "Использовать средства",
+                flagUpdate.getId());
+        taskManager.addSubtask(flagUpdateSubtask1);
+        taskManager.addSubtask(flagUpdateSubtask2);
+        System.out.println(flagUpdate);
+        flagUpdateSubtask2.setStatus(Status.DONE);
+        taskManager.updateSubtask(flagUpdateSubtask2);
+        System.out.println(flagUpdate);
     }
 }
---
+++ src/Subtask.java
@@ -0,0 +1,24 @@
+public class Subtask extends Task {
+
+    private final int epicID;
+
+    public Subtask(String name, String description, int epicID) {
+        super(name, description);
+        this.epicID = epicID;
+    }
+
+    public int getEpicID() {
+        return epicID;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", id=" + getId() +
+                ", epicID=" + epicID +
+                ", status=" + getStatus() +
+                '}';
+    }
+}
